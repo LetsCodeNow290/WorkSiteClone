33125701e77365179783ba3a4938d11a0974818e
LetsCodeNow290 <jmckittrick@comcast.net>
2020-05-27 10:11:28 -0400

    added date picker

--- safe/forms.py
+++ safe/forms.py
@@ -4,13 +4,17 @@ from components.models import Drug
 import django_filters
 from django.forms import modelformset_factory, ModelChoiceField, Textarea
 
+class DateInput(forms.DateInput):
+  input_type = 'date'
+
 class DrugSearch(django_filters.FilterSet):
-    start_date = django_filters.DateFilter(field_name='date_created', lookup_expr='gte', label='Records After', widget=forms.TextInput(attrs={'placeholder': 'MM/DD/YYYY'}))
-    exact_date = django_filters.DateFilter(field_name='date_created', lookup_expr='icontains', label='Records On', widget=forms.TextInput(attrs={'placeholder': 'MM/DD/YYYY'}))
+    start_date = django_filters.DateFilter(field_name='date_created', lookup_expr='gte', label='Records After', widget=DateInput())
+    exact_date = django_filters.DateFilter(field_name='date_created', lookup_expr='icontains', label='Records On', widget=DateInput())
 
     class Meta:
        model = Safe
        fields = ['drug_name', 'start_date', 'exact_date']
+       
 
 class SafeCheckForm(forms.ModelForm):
     class Meta:
